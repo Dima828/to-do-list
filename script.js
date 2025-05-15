669800d5737ef35cd92fb447bcde10601ec7a639
Dmitriy Shvets <dimasflow89938@gmail.com>
2025-05-15 17:28:45 +0300

    Finish

--- script.js
+++ script.js
@@ -1,21 +1,66 @@
-const inputelement = document.getElementById('title')
+const inputElement = document.getElementById('title')
 const createBtn = document.getElementById('create')
 const listElement = document.getElementById('list')
 
+const notes = [
+    {
+        title: '123',
+        complited: false,
+    },
+    {
+        title: '321',
+        complited: true,
+    },
+]
+
+function render(){
+    listElement.innerHTML = ''
+    if(notes.length == 0){
+        listElement.innerHTML = '<p>Нет элементов</p>'
+    }
+    for(let i = 0; i < notes.length; i++){
+        listElement.insertAdjacentHTML('beforeend', getNoteTemplate(notes[i], i))
+    }
+}
+
+render()
+
 createBtn.onclick = function(){
-    if(inputelement.value.length === 0){
+    if(inputElement.value.length === 0){
         return
     }else{
-        listElement.insertAdjacentHTML('beforeend', `
-        <li class="list-group-item">
-            <span>${inputelement.value}</span>
-                <span>
-                    <span class="btn btn-small btn-success">&check;</span>
-                    <span class="btn btn-small btn-danger">&times;</span>
-                </span>
-        </li>`)
-
-        inputelement.value = ''
+        const newNote = {
+            title: inputElement.value,
+            complited: false,
+        }
+        notes.push(newNote)
+        render()
+        inputElement.value = ''
     }  
-    
+}
+
+listElement.onclick = function(event){
+    if(event.target.dataset.index){
+        const index = Number(event.target.dataset.index)
+        const type = event.target.dataset.type
+        if(type == 'toggle'){
+            notes[index].complited = !notes[index].complited
+        }else if (type == 'remove'){
+            notes.splice(index, 1)
+        }
+}
+
+render()
+
+}
+
+function getNoteTemplate(note, index){
+    return `
+        <li class="list-group-item">
+            <span class="${note.complited ? 'text-decoration-line-through' : ''}">${note.title}</span>
+                <span>
+                    <span class="btn btn-small btn-${note.complited ? 'warning' : 'success'}" data-index="${index}" data-type="toggle">&check;</span>
+                    <span class="btn btn-small btn-danger" data-index="${index}" data-type="remove">&times;</span>
+                </span>
+        </li>`
 }
